ad879f3f7b890ffb04e51367bb5d8ba16a3e1980
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-08-20 06:18:49 +0530

    uc1

---
+++ FibonacciSeries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalOne
+{
+    internal class FibonacciSeries
+    {
+        public static void DisplayFibonacciSeries()
+        {
+            int a = 0;
+            int b = 1;
+            int c;
+            int i;
+            Console.WriteLine("Fibonacci series : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a + " " + b + " ");
+            for (i = 2; i < n; ++i)
+            {
+                c = a + b;
+                Console.WriteLine(c + " ");
+                a = b;
+                b = c;
+            }
+
+        }
+    }
+}
+    
